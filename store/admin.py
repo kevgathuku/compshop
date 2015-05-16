e713a43f0a9ef2a1c0465aa81ce3f899a6971272
Kevin Ndung'u <kevgathuku@gmail.com>
2015-05-16 13:40:28 +0300

    Add basic store admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image, Product, Review, Specification
+
+
+class ImageInline(admin.StackedInline):
+    model = Image
+
+
+class SpecificationInline(admin.StackedInline):
+    model = Specification
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+    )
+    inlines = [ImageInline, SpecificationInline, ]
+    search_fields = ['name', 'description',]
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    fields = ['product', 'name', 'title', 'rating', 'text']
+
+admin.site.register(Image)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Specification)
