3db8bf3a8da0ac3e5b525eb418fb4f4c572d9ec0
Kevin Ndung'u <kevgathuku@gmail.com>
2015-05-29 12:34:37 +0300

    Add Homepage view and URL

--- compshop/urls.py
+++ compshop/urls.py
@@ -18,6 +18,9 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from store.views import ProductList
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', ProductList.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
