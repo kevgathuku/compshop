e3773b36713d65b058aebd25e4f1b5fa19d53f6e
Kevin Ndung'u <kevgathuku@gmail.com>
2015-05-29 13:06:27 +0300

    Add ProductCatalogue view and URL

--- compshop/urls.py
+++ compshop/urls.py
@@ -18,9 +18,10 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from store.views import ProductList
+from store.views import ProductCatalogue, ProductList
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', ProductList.as_view(), name='home'),
+    url(r'^catalogue/$', ProductCatalogue.as_view(), name='catalogue'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -6,3 +6,8 @@ from .models import Product
 class ProductList(ListView):
     model = Product
     template_name = 'store/index.html'
+
+
+class ProductCatalogue(ListView):
+    model = Product
+    template_name = 'store/catalogue.html'
