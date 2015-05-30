320bd8b1f72b86a8ae0edd43cbed945c39433ae1
Kevin Ndung'u <kevgathuku@gmail.com>
2015-05-30 09:00:12 +0300

    Add debug toolbar to local settings

--- settings/local.py
+++ settings/local.py
@@ -9,6 +9,8 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGE THIS!!!'
 
+INSTALLED_APPS += ('debug_toolbar',)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
