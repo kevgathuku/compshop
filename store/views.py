95ce0bd20250ce5334925b44c109c7e4004cfc08
Kevin Ndung'u <kevgathuku@gmail.com>
2015-05-30 09:11:20 +0300

    Fill in Product Detail Template

--- store/views.py
+++ store/views.py
@@ -16,3 +16,9 @@ class ProductCatalogue(ListView):
 class ProductDetail(DetailView):
     model = Product
     pk_url_kwarg = 'product_id'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        context['images'] = self.object.images.all()
+        context['main_image'] = self.object.images.first()
+        return context
