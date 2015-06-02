883ecd2b27a673240159eec9a7b619db8333b95a
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-02 15:05:23 +0300

    Configure Djrill settings

--- settings/base.py
+++ settings/base.py
@@ -116,3 +116,10 @@ STATIC_ROOT = 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 MEDIA_URL = '/media/'
+
+# Djrill Settings
+MANDRILL_API_KEY = os.environ['MANDRILL_API_KEY']
+
+EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
+
+DEFAULT_FROM_EMAIL = "<Comptronics> kevgathuku@gmail.com"
