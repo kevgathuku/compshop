53a14578f3037ecc798cc314cd9b0aa7a07de720
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-03 13:11:15 +0300

    Specify verbose_name_plural for Category

--- store/models.py
+++ store/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = AutoSlugField(populate_from='name', unique=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
