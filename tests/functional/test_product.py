21cb063ec63792ddeb45a62570a8565c69f2091b
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-05 14:43:46 +0300

    Create product in setUp() method for DRY
    Create the product to be reused in the test methods in the setUp method
    that is run before each test method

--- tests/functional/test_product.py
+++ tests/functional/test_product.py
@@ -5,26 +5,25 @@ from store.tests.factories import *
 
 class ProductTest(FunctionalTest):
 
-    def test_product_navigation(self):
+    def setUp(self):
+        super(ProductTest, self).setUp()
         # Create a product
-        product = ProductFactory.create()
+        self.product = ProductFactory.create()
 
+    def test_product_navigation(self):
         # Get the product detail page
-        self.browser.get(self.live_server_url + product.get_absolute_url())
+        self.browser.get(self.live_server_url + self.product.get_absolute_url())
 
         # Assert that the title is as expected
-        self.assertIn(product.name, self.browser.title)
+        self.assertIn(self.product.name, self.browser.title)
 
 
     def test_product_navigation_from_homepage(self):
-        # Create a sample product
-        product1 = ProductFactory.create()
-
         # Get the homepage
         self.browser.get(self.live_server_url)
 
         # Navigate to the Product Page
-        self.browser.find_element_by_link_text(product1.name).click()
+        self.browser.find_element_by_link_text(self.product.name).click()
 
         # Assert that the page is the one expected
-        self.assertIn(product1.name, self.browser.title)
+        self.assertIn(self.product.name, self.browser.title)
