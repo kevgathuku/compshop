c5e15a4692e469b5f67c55e6bbc2344aa91b954c
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-05 17:26:30 +0300

    Add test for the Review model saving blank items

--- store/tests/factories.py
+++ store/tests/factories.py
@@ -26,7 +26,6 @@ class ReviewFactory(factory.django.DjangoModelFactory):
         model = models.Review
 
     name = factory.Sequence(lambda n: "Reviewer_%d" % n)
-    title = factory.Sequence(lambda n: "Review_%d" % n)
     rating = factory.fuzzy.FuzzyChoice(dict(models.Review.RATINGS_CHOICES).keys())
     text = factory.fuzzy.FuzzyText()
     product = factory.SubFactory(ProductFactory)
