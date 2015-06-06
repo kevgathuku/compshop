b8e53301cccaf8333874df5387ab487217aade91
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-06 11:53:19 +0300

    Use the ModelForm's name field
    Don't define a new name field in the form
    Assert that form.save() saves the name and product fields correctly

--- store/forms.py
+++ store/forms.py
@@ -4,19 +4,14 @@ from .models import Review
 
 
 class ReviewForm(forms.models.ModelForm):
-    name = forms.CharField(
-        max_length=30,
-        widget=forms.TextInput(
-            attrs={
+    class Meta:
+        model = Review
+        fields = ['name', 'rating', 'text','product']
+        widgets = {
+            'name': forms.TextInput(attrs={
                 'placeholder': 'Your Name',
                 'class': 'form-control',
                 }),
-        )
-
-    class Meta:
-        model = Review
-        fields = ['rating', 'text','product']
-        widgets = {
             'text': forms.Textarea(
                 attrs={
                     'placeholder': 'Your Review',
--- store/tests/test_forms.py
+++ store/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from store.forms import ReviewForm
+from store.models import Review
 from .factories import *
 
 
@@ -10,8 +11,20 @@ class ReviewFormTest(TestCase):
         p1 = ProductFactory.create()
 
         form = ReviewForm(
-            data={'name':'Kevin', 'text': '', 'product':p1.id})
+            data={'name':'', 'text': '', 'product':p1.id})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'],["Please fill in the review"])
         self.assertEqual(form.errors['rating'],["Please leave a rating"])
 
+
+    def test_form_save_handles_saving_to_a_list(self):
+        prod = ProductFactory.create()
+
+        form = ReviewForm(
+            data={'name':'Kevin', 'text': 'Review', 'rating': 3, 'product':prod.id})
+
+        new_review = form.save()
+
+        self.assertEqual(new_review, Review.objects.first())
+        self.assertEqual(new_review.name, 'Kevin')
+        self.assertEqual(new_review.product, prod)
