cee27cb246bc9e0e322ddc9a7e0c06e8920ca130
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-11 08:26:53 +0300

    Test that categories are displayed on sidebar
    - Add functional test to ensure that correct categories are displayed on
    the homepage sidebar

--- tests/functional/test_home_page.py
+++ tests/functional/test_home_page.py
@@ -1,3 +1,4 @@
+from store.tests.factories import CategoryFactory, ProductFactory
 from .base import FunctionalTest
 
 
@@ -7,6 +8,25 @@ class HomePageTest(FunctionalTest):
         self.browser.get(self.live_server_url)
 
         self.assertIn("Home", self.browser.title)
+
+    def test_category_sidebar_display(self):
+        cat1 = CategoryFactory.create()
+        cat2 = CategoryFactory.create()
+
+        product1 = ProductFactory.create(category=cat1)
+
+        self.browser.get(self.live_server_url)
+
+        menu = self.browser.find_element_by_id('main_menu')
+
+        self.assertIn(
+            cat1.name,
+            menu.text,
+            "Categories with products should be visible on the sidebar")
+        self.assertNotIn(
+            cat2.name,
+            menu.text,
+            "Categories without products should not be visible on the sidebar")
 
 
     def test_navbar_links(self):
