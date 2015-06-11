0ce2c6b70ab201c2726a2f6dffa2ebb882ea3fc6
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-11 09:29:42 +0300

    Remove custom TEMPATES setting in test settings

--- settings/test.py
+++ settings/test.py
@@ -12,21 +12,4 @@ DEBUG = True
 
 DATABASES['default'] = dj_database_url.config()
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-            'debug': True,
-        },
-    },
-]
-
 SECRET_KEY = 'TESTing SecRet KeY'
