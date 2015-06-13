57244fe127c5dae8fd2d2bc19de9a6cdbe7bdf13
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-13 12:53:04 +0300

    Fix context processor tests
    Create at least one featured product before running tests

--- store/tests/test_context_processor.py
+++ store/tests/test_context_processor.py
@@ -4,17 +4,44 @@ Tests for Custom context processors.
 import os
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.test import TestCase, override_settings
 
+from .factories import ProductFactory
 
 class ProductCategoriesContextProcessorTests(TestCase):
     """
     Tests for the ``store.context_processors.product_categories`` processor.
     """
 
-    def test_custom_context_exists(self):
+    def setUp(self):
+        super(ProductCategoriesContextProcessorTests, self).setUp()
+        # Create a fearued product so that selecting a random featured image
+        # in the template doesn't raise an error
+        ProductFactory.create(featured=True)
+
+    @override_settings(
+    TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [os.path.join(settings.BASE_DIR, 'templates')],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                        'store.context_processors.featured_products',
+                        'store.context_processors.product_categories',
+                    ],
+                },
+            },
+        ]
+    )
+    def test_custom_context_exists_if_context_processor_included(self):
         # Get the homepage
-        response = self.client.get('/')
+        response = self.client.get(reverse('home'))
         self.assertIn('product_categories', response.context)
 
     @override_settings(
@@ -29,6 +56,7 @@ class ProductCategoriesContextProcessorTests(TestCase):
                         'django.template.context_processors.request',
                         'django.contrib.auth.context_processors.auth',
                         'django.contrib.messages.context_processors.messages',
+                        'store.context_processors.featured_products',
                     ],
                 },
             },
@@ -36,5 +64,5 @@ class ProductCategoriesContextProcessorTests(TestCase):
     )
     def test_custom_context_does_not_exist_if_not_included_in_settings(self):
         # Get the homepage
-        response = self.client.get('/')
+        response = self.client.get(reverse('home'))
         self.assertNotIn('product_categories', response.context)
