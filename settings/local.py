eadeb03838d857ffaca5c4dba293912dbd88a499
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-13 13:09:26 +0300

    Remove debug_toolbar from local settings

--- settings/local.py
+++ settings/local.py
@@ -9,4 +9,4 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGE THIS!!!'
 
-INSTALLED_APPS += ('debug_toolbar', 'django_seed',)
+INSTALLED_APPS += ('django_seed',)
