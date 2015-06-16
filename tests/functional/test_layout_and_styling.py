6941d9048a8c630244bb48100864872b35a1a307
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-16 09:09:48 +0300

    Fix bootstrap and jQuery link checking in homepage

--- tests/functional/test_layout_and_styling.py
+++ tests/functional/test_layout_and_styling.py
@@ -1,5 +1,3 @@
-import os
-
 from .base import FunctionalTest
 
 
@@ -8,14 +6,19 @@ class LayoutStylingTest(FunctionalTest):
     def test_bootstrap_links_loaded_successfully(self):
         self.browser.get(self.live_server_url)
 
-        self.assertIn(
-            "//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css",
-            self.browser.page_source.strip())
+        links = [link.get_attribute("href")
+                 for link in self.browser.find_elements_by_tag_name('link')]
+        scripts = [script.get_attribute("src")
+                   for script in self.browser.find_elements_by_tag_name('script')]
 
-        self.assertIn(
-            "//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js",
-            self.browser.page_source.strip())
+        self.assertTrue(
+            ["//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"
+             in link for link in links])
 
-        self.assertIn(
-            '//code.jquery.com/jquery.min.js',
-            self.browser.page_source.strip())
+        self.assertTrue(
+            ["//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"
+             in link for link in links])
+
+        self.assertTrue(
+            ["//code.jquery.com/jquery.min.js"
+             in link for link in scripts])
