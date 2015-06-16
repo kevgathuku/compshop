a149f0d55dc10a7be2418e7f1c7518ed1b834ced
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-16 09:52:01 +0300

    Define SERVER_EMAIL in prod settings

--- settings/prod.py
+++ settings/prod.py
@@ -14,6 +14,8 @@ ALLOWED_HOSTS = ['*']
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+SERVER_EMAIL = "<Comptronics Error> kevgathuku@gmail.com"
+
 ADMINS = (
     ("Kevin Ndung'u", 'kevgathuku@gmail.com'),
 )
