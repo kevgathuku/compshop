41f1f3502b6694383744774143560e7175c3e2a1
Kevin Ndung'u <kevgathuku@gmail.com>
2015-06-27 15:06:19 +0300

    Use explicit wait to test successful review posting

--- tests/functional/base.py
+++ tests/functional/base.py
@@ -1,5 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -10,3 +12,9 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    # Explicit wait for an element not to be visible
+    def wait_for_element_with_id_invisible(self, element_id):
+        WebDriverWait(self.browser, timeout=10).until_not(
+                lambda b: b.find_element_by_id(element_id).is_displayed()
+            )
