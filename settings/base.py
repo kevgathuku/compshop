35ae1126b349d884c166f59991b088d884521611
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-01 14:57:49 +0300

    Upgrade to Django 1.9

--- settings/base.py
+++ settings/base.py
@@ -67,6 +67,21 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'compshop.urls'
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
